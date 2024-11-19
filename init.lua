23dd170d86843446f915d1ceb334b8a0f5a455ab
Qeuroal <12997837+qeuroal@user.noreply.gitee.com>
2024-11-19 15:16:35 +0800

    init

---
+++ init.lua
@@ -0,0 +1,4 @@
+-- bootstrap lazy.nvim, PithyVim and your plugins
+require("config.lazy")
+
+
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Add any additional keymaps here
+
+
