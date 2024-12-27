a1a4960c70f027fc969006ee4e8d5c50686424a3
Qeuroal <Qeuroal@163.com>
2024-12-27 13:32:30 +0800

    extras plugins

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -1,7 +1,7 @@
 -- default extras
 return {
-  { import = "pithyvim.plugins.extras.coding.luasnip", },
   { import = "pithyvim.plugins.extras.ui.mini-indentscope" },
+  -- { import = "pithyvim.plugins.extras.coding.luasnip", },
   -- { import = "pithyvim.plugins.extras.lang.clangd", },
   -- { import = "pithyvim.plugins.extras.lang.python", },
   -- { import = "pithyvim.plugins.extras.lang.markdown", },
