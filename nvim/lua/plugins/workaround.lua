4526a6eeac866cebf7f9f7549464ff4ce022d2ad
Qeuroal <Qeuroal@163.com>
2025-05-11 14:53:43 +0800

    fix: mason

--- nvim/lua/plugins/workaround.lua
+++ nvim/lua/plugins/workaround.lua
@@ -1,6 +1,6 @@
 return {
   -- fix, by simply pinning the versions
   -- bug: Failed to run config for nvim-lspconfig after mason upgrade
-  { "mason-org/mason.nvim", version = "^1.11.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.32.0" },
+  -- { "mason-org/mason.nvim", version = "^1.11.0" },
+  -- { "mason-org/mason-lspconfig.nvim", version = "^1.32.0" },
 }
