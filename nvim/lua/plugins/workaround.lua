d2c15619b7539d58cf7c6c72cac5c7968628c990
Qeuroal <Qeuroal@163.com>
2025-05-11 14:54:20 +0800

    fix: mason

--- nvim/lua/plugins/workaround.lua
+++ nvim/lua/plugins/workaround.lua
@@ -3,4 +3,5 @@ return {
   -- bug: Failed to run config for nvim-lspconfig after mason upgrade
   -- { "mason-org/mason.nvim", version = "^1.11.0" },
   -- { "mason-org/mason-lspconfig.nvim", version = "^1.32.0" },
+  { "Qeuroal/PithyVim", import = "pithyvim.plugins", branch = "fix_mason" },
 }
