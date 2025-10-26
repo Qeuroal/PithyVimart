6282354ff9cdfbc711f106cddc26fab303386ee5
Qeuroal <Qeuroal@163.com>
2025-10-26 13:40:44 +0800

    feat: 改进SSH配置导入逻辑并修复条件判断语法

--- scripts/config_dotfiles.sh
+++ scripts/config_dotfiles.sh
@@ -2,6 +2,16 @@
 
 source ${PWD}/scripts/color_print.sh
 
+function is_gnu_sed() {
+    if ! command -v sed > /dev/null; then
+        echo 0
+    elif [[ "$(uname)" == 'Darwin' && "$(command -v sed | grep -c 'gnu-sed')" -eq 0 ]]; then
+        echo 0
+    else
+        echo 1
+    fi
+}
+
 function import_gitconfig ()
 {
     if [ $# -ne 1 ]; then
@@ -9,21 +19,33 @@ function import_gitconfig ()
         return
     fi
     local destfilepath="$1"
-    color_print "info" "Success to import configure to $destfilepath"
+
     echo '[include]' | tee -a ${destfilepath} > /dev/null
     echo '	path = ~/.config/pithy_configs/.gitconfig' | tee -a ${destfilepath} > /dev/null
+    color_print "info" "Success to import configure to $destfilepath"
 }
 
-function import_sshconfig ()
+function selfimport_sshconfig ()
 {
     if [ $# -ne 1 ]; then
         color_print "error" "parameter error: expect 1 parameters."
         return
     fi
     local destfilepath="$1"
-    color_print "info" "Success to import configure to $destfilepath"
+    [ ! -e "$destfilepath" ] && echo '' >> $destfilepath
 
-    echo 'Include ~/.config/pithy_configs/.sshconfig' | tee -a ${destfilepath} > /dev/null
+    if [[ "$(is_gnu_sed)" -eq 1 ]]; then
+        if [[ ! -e "$destfilepath.default.bak" ]]; then
+            sed -i.default.bak -e '1i\# import sshconfig' -e '1i\Include ~/.config/pithy_configs/.sshconfig' $destfilepath
+        else
+            sed -i.sed.bak -e '1i\# import sshconfig' -e '1i\Include ~/.config/pithy_configs/.sshconfig' $destfilepath
+        fi
+        color_print "info" "Success to import configure to $destfilepath"
+    else
+        echo 'Include ~/.config/pithy_configs/.sshconfig' | tee -a ${destfilepath} > /dev/null
+        color_print "warning" "WARNING: The .ssh/config configuration should be placed at the beginning of the file."
+        color_print "info" "HALF success to import configure to $destfilepath"
+    fi
 }
 
 function import_tmux_conf ()
@@ -33,9 +55,9 @@ function import_tmux_conf ()
         return
     fi
     local destfilepath="$1"
-    color_print "info" "Success to import configure to $destfilepath"
 
     echo 'source-file ~/.config/pithy_configs/.tmux.conf' | tee -a ${destfilepath} > /dev/null
+    color_print "info" "Success to import configure to $destfilepath"
 }
 
 function import_configure ()
@@ -56,7 +78,8 @@ function import_configure ()
     [ ! -f ${destfilepath} ] && touch ${destfilepath}
 
     if [ `cat ${destfilepath} | grep -c "$import_signal"` = 0 ]; then
-        echo "$import_signal" | tee -a ${destfilepath} > /dev/null
+        # selfimport_* 函数自行导入 import signal
+        [[ ! "$hook_func" =~ "selfimport_" ]] && echo "$import_signal" | tee -a ${destfilepath} > /dev/null
         $hook_func ${destfilepath}
         return 0
     fi
@@ -67,8 +90,7 @@ function import_configure ()
 function main ()
 {
     import_configure "$HOME/.gitconfig" "; import gitconfig" "import_gitconfig"
-    import_configure "$HOME/.ssh/config" "# import sshconfig" "import_sshconfig"
-    [ "$?" = "0" ] && color_print "warning" "WARNING: The .ssh/config configuration should be placed at the beginning of the file."
+    import_configure "$HOME/.ssh/config" "# import sshconfig" "selfimport_sshconfig"
     import_configure "$HOME/.tmux.conf" "# import tmux.conf" "import_tmux_conf"
 
     return 0
