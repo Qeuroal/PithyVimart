2ea42695d323f0e5773b11e564176b3231629469
Qeuroal <Qeuroal@163.com>
2025-10-26 14:32:39 +0800

    refactor: 重构配置文件导入函数以支持通用导入逻辑

--- scripts/config_dotfiles.sh
+++ scripts/config_dotfiles.sh
@@ -12,40 +12,59 @@ function is_gnu_sed() {
     fi
 }
 
-function import_gitconfig ()
+function sed_improt_prelude ()
 {
-    if [ $# -ne 1 ]; then
+    if [ $# -ne 3 ]; then
         color_print "error" "parameter error: expect 1 parameters."
         return
     fi
     local destfilepath="$1"
+    local import_signal="$2"
+    local import_content="$3"
 
-    echo '[include]' | tee -a ${destfilepath} > /dev/null
-    echo '	path = ~/.config/pithy_configs/.gitconfig' | tee -a ${destfilepath} > /dev/null
-    color_print "info" "Success to import configure to $destfilepath"
-}
-
-function selfimport_sshconfig ()
-{
-    if [ $# -ne 1 ]; then
-        color_print "error" "parameter error: expect 1 parameters."
-        return
-    fi
-    local destfilepath="$1"
     [ ! -e "$destfilepath" ] && echo '' >> $destfilepath
-
     if [[ "$(is_gnu_sed)" -eq 1 ]]; then
         if [[ ! -e "$destfilepath.default.bak" ]]; then
-            sed -i.default.bak -e '1i\# import sshconfig' -e '1i\Include ~/.config/pithy_configs/.sshconfig' -e '1{x;p;x;}' $destfilepath
+            # 添加空行使用 -e '1{x;p;x;}'
+            sed -i.default.bak -e "1i\\$import_signal" -e "1i$import_content" $destfilepath
         else
-            sed -i.sed.bak -e '1i\# import sshconfig' -e '1i\Include ~/.config/pithy_configs/.sshconfig' -e '1{x;p;x;}' $destfilepath
+            sed -i.sed.bak -e "1i\\$import_signal" -e "1i$import_content" $destfilepath
         fi
         color_print "info" "Success to import configure to $destfilepath"
     else
-        echo 'Include ~/.config/pithy_configs/.sshconfig' | tee -a ${destfilepath} > /dev/null
-        color_print "warning" "WARNING: The .ssh/config configuration should be placed at the beginning of the file."
+        echo "$import_content" | tee -a ${destfilepath} > /dev/null
+        color_print "warning" "WARNING: The $destfilepath configuration should be placed at the beginning of the file."
         color_print "info" "HALF success to import configure to $destfilepath"
     fi
+
+}
+
+function preimport_gitconfig ()
+{
+    if [ $# -ne 2 ]; then
+        color_print "error" "parameter error: expect 1 parameters."
+        return
+    fi
+    local destfilepath="$1"
+    local import_signal="$2"
+
+    sed_improt_prelude "$destfilepath" "$import_signal" '[include]\n	path = ~/.config/pithy_configs/.gitconfig'
+
+    # echo '[include]\n	path = ~/.config/pithy_configs/.gitconfig' | tee -a ${destfilepath} > /dev/null
+    # echo '' | tee -a ${destfilepath} > /dev/null
+    # color_print "info" "Success to import configure to $destfilepath"
+}
+
+function preimport_sshconfig ()
+{
+    if [ $# -ne 2 ]; then
+        color_print "error" "parameter error: expect 1 parameters."
+        return
+    fi
+    local destfilepath="$1"
+    local import_signal="$2"
+
+    sed_improt_prelude "$destfilepath" "$import_signal" 'Include ~/.config/pithy_configs/.sshconfig'
 }
 
 function import_tmux_conf ()
@@ -55,6 +74,7 @@ function import_tmux_conf ()
         return
     fi
     local destfilepath="$1"
+    local import_signal="$2"
 
     echo 'source-file ~/.config/pithy_configs/.tmux.conf' | tee -a ${destfilepath} > /dev/null
     color_print "info" "Success to import configure to $destfilepath"
@@ -78,9 +98,9 @@ function import_configure ()
     [ ! -f ${destfilepath} ] && touch ${destfilepath}
 
     if [ `cat ${destfilepath} | grep -c "$import_signal"` = 0 ]; then
-        # selfimport_* 函数自行导入 import signal
-        [[ ! "$hook_func" =~ "selfimport_" ]] && echo '' | tee -a ${destfilepath} > /dev/null; echo "$import_signal" | tee -a ${destfilepath} > /dev/null
-        $hook_func ${destfilepath}
+        # preimport_* 函数自行导入 import signal
+        [[ ! "$hook_func" =~ "preimport_" ]] && (echo '' | tee -a ${destfilepath} > /dev/null; echo "$import_signal" | tee -a ${destfilepath} > /dev/null)
+        $hook_func "${destfilepath}" "${import_signal}"
         return 0
     fi
 
@@ -89,8 +109,8 @@ function import_configure ()
 
 function main ()
 {
-    import_configure "$HOME/.gitconfig" "; import gitconfig" "import_gitconfig"
-    import_configure "$HOME/.ssh/config" "# import sshconfig" "selfimport_sshconfig"
+    import_configure "$HOME/.gitconfig" "; import gitconfig" "preimport_gitconfig"
+    import_configure "$HOME/.ssh/config" "# import sshconfig" "preimport_sshconfig"
     import_configure "$HOME/.tmux.conf" "# import tmux.conf" "import_tmux_conf"
 
     return 0
