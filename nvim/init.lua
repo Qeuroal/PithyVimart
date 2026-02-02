0cb25a0862ffa2375974357c2085ebc7e0411903
Qeuroal <Qeuroal@163.com>
2026-02-02 17:25:29 +0800

    feat: 添加调试工具和VSCode环境检测

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,24 @@
+if vim.env.VSCODE then
+  vim.g.vscode = true
+end
+
+if vim.loader then
+  vim.loader.enable()
+end
+
+_G.dd = function(...)
+  require("snacks.debug").inspect(...)
+end
+_G.bt = function(...)
+  require("snacks.debug").backtrace()
+end
+_G.p = function(...)
+  require("snacks.debug").profile(...)
+end
+vim._print = function(_, ...)
+  dd(...)
+end
+
 -- bootstrap lazy.nvim, PithyVim and your plugins
 require("config.lazy")
 
